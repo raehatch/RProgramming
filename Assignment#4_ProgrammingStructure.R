c3ffbad04193c886d68484d56368da1e1e6ff445
Rachel Hatcherian <rachel.hatcherian@gmail.com>
2021-02-07 16:39:29 -0500

    Create Assignment#4_ProgrammingStructure.R

---
+++ Assignment#4_ProgrammingStructure.R
@@ -0,0 +1,44 @@
+#install.packages("reshape")
+library(reshape)
+library(ggplot2)
+library(gridExtra)
+
+#Generate individual data points
+freq <- c(0.6,0.3,0.4,0.4,0.2,0.6,0.3,0.4,0.9,0.2)
+bp <- c(103,87,32,42,59,109,78,205,135,176)
+first <- c(1,1,1,1,0,0,0,0,NA,1)
+second <- c(0,0,1,1,0,0,1,1,1,1)
+final <- c(0,1,0,1,0,1,0,1,1,1)
+
+#Combine into data frame
+df <- cbind(freq,bp,first,second,final)
+
+#Remove NA's
+df <- as.data.frame(na.omit(df))
+
+#Melt data for visualization
+dfNew <- melt(df,id ="bp",measure.vars = c('first','second','final'))
+
+#Add categorical for high/low for viz
+dfNew$level <- ifelse(dfNew$value==1, "High","Low")
+
+#A: Create a boxplot
+boxplot <- ggplot(dfNew,aes(x=variable,y=bp,fill=level)) + 
+  geom_boxplot() +
+  ylab("Blood Pressure") +
+  xlab("Asessment Visit") +
+  ggtitle("Blood Presure by Visit")
+  
+#B: Create a histogram
+histo <- ggplot(df,aes(bp)) + 
+  geom_histogram(bins=9,color="steelblue",fill="white") +
+  xlab("Blood Pressure") +
+  ggtitle("Blood Pressure Histogram")
+
+#View visualizations
+grid.arrange(boxplot,histo,
+             ncol=2,
+             nrow=1)
+
+
+
